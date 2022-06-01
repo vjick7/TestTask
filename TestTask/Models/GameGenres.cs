fff30833fb59d9162031cd142ec2ec7b2d774f90
Dmitry Lukyanov <vjick7@gmail.com>
2022-06-01 20:42:39 +0300

    ICollection to IQueryable

--- TestTask/Models/GameGenres.cs
+++ TestTask/Models/GameGenres.cs
@@ -11,17 +11,5 @@ namespace TestTask.Models
         public Game Game { get; set; }
         public int GenreId { get; set; }
         public Genre Genre { get; set; }
-
-        //public string GenresString()
-        //{
-        //    if (Genres.Count() == 0)
-        //        return "Genre not selected";
-        //    StringBuilder str = new StringBuilder();
-        //    foreach (GameGenres gameGenre in Genres)
-        //    {
-        //        str.Append($"{gameGenre.Genre.Title}<br />");
-        //    }
-        //    return str.ToString();
-        //}
     }
 }
